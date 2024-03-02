045593148b82f70399bf1258613473d2c9290ccb
Louis Xia <23242389+xialouis1@users.noreply.github.com>
2024-03-02 04:55:40 -0500

    Update app.js to include countDown()
    Created a function called countDown which accepts a parameter, num. We decrement num every second until num equals 0 and print "DONE!"

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+function countDown(num) {
+    if(typeof num !== 'number') {
+        throw new Error("Parameter must be a number.");
+    }
+
+    const IntervalID_countdown = setInterval(function() {
+        num--;
+        console.log(num);
+
+        if(num <= 1) {
+            clearInterval(IntervalID_countdown);
+            console.log("DONE!");
+        } 
+    }, 
+    1000);
+}
